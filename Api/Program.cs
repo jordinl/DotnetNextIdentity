6402058f260c66865a3ac5cba4efb4df890ccdd2
Jordi Noguera <jordinoguera83@gmail.com>
2025-01-27 15:49:10 +0100

    Add scalar to visualize API documentation

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +30,7 @@ if (app.Environment.IsDevelopment())
         db.Database.Migrate();
     }
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
